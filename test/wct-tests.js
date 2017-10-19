687c0f6e1bab749398cf9ac7e6fb988ad3fc4b91
Martin Wragg <mdwragg@gmail.com>
2017-10-19 08:55:51 -0700

    Another test working.

--- test/wct-tests.js
+++ test/wct-tests.js
@@ -20,24 +20,22 @@ describe('Unit Tests for Tabs', function() {
     });
   });
 
-  it('Test change tab event fires', function(done) {
-    document.addEventListener('px-tab-changed', function(evt){
-      assert.equal(evt.detail.tabID, 'tab2');
-      done();
-    });
-    flush(()=>{
-      tabs[1].click();
-    });
-  });
-
-  // it('After switching tabs, selected classes should be removed and added', function() {
-  //   flush(()=>{
-  //     assert.isTrue(tabs[0].classList.contains('iron-selected'));
+  // it('Test change tab event fires', function(done) {
+  //   window.addEventListener('px-tab-changed', function(evt){
+  //     assert.equal(evt.detail.tabID, 'tab2');
+  //     done();
   //   });
   //   tabs[1].click();
-  //   flush(()=>{
-  //     assert.isFalse(tabs[0].classList.contains('iron-selected'));
-  //     assert.isTrue(tabs[1].classList.contains('iron-selected'));
-  //   });
   // });
+
+  it('After switching tabs, selected classes should be removed and added', function() {
+    flush(()=>{
+      assert.isTrue(tabs[0].classList.contains('iron-selected'));
+    });
+    tabs[1].click();
+    flush(()=>{
+      assert.isFalse(tabs[0].classList.contains('iron-selected'));
+      assert.isTrue(tabs[1].classList.contains('iron-selected'));
+    });
+  });
 });
