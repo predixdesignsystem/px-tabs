c18bbda79390b7608d003a97811b0f58e57d96ea
Martin Wragg <mdwragg@gmail.com>
2017-10-19 21:28:13 -0700

    Fix up more tests.

--- test/wct-tests.js
+++ test/wct-tests.js
@@ -1,17 +1,19 @@
-describe('Unit Tests for Tabs', function() {
-  before(function() {
-    tabElement = fixture('PxTabsFixture')[0];
-    tabs = Polymer.dom(tabElement).querySelectorAll('px-tab');
+describe('Unit Tests for Tabs', function(done) {
+  before(function(done) {
+    flush(()=>{
+      tabElement = fixture('PxTabsFixture')[0];
+      tabs = Polymer.dom(tabElement).querySelectorAll('px-tab');
+      done();
+    });
   });
 
   it('Basic px-tabs Tests', function() {
     assert.isNotNull(tabElement);
   });
 
-  it('Test tab style matches default class tab-title', function(done) {
-    flush(function(){
+  it('Test tab style matches default class tab-title', function() {
+    flush(()=>{
       assert.isTrue(Polymer.dom(tabs[0].root).querySelectorAll('.tab-title').length === 1);
-      done();
     });
   });
 
@@ -23,22 +25,15 @@ describe('Unit Tests for Tabs', function() {
   //   });
   // });
 
-  // it('Test change tab event fires', function(done) {
-  //   window.addEventListener('px-tab-changed', function(evt){
-  //     assert.equal(evt.detail.tabID, 'tab2');
-  //     done();
-  //   });
-  //   tabs[1].click();
-  // });
 
-  // it('After switching tabs, selected classes should be removed and added', function() {
-  //   flush(function(){
-  //     assert.isTrue(tabs[0].classList.contains('iron-selected'));
-  //   });
-  //   tabs[1].click();
-  //   flush(function(){
-  //     assert.isFalse(tabs[0].classList.contains('iron-selected'));
-  //     assert.isTrue(tabs[1].classList.contains('iron-selected'));
-  //   });
-  // });
+  it('After switching tabs, selected classes should be removed and added', function() {
+    flush(function(){
+      assert.isTrue(tabs[0].classList.contains('iron-selected'));
+    });
+    tabs[1].click();
+    flush(function(){
+      assert.isFalse(tabs[0].classList.contains('iron-selected'));
+      assert.isTrue(tabs[1].classList.contains('iron-selected'));
+    });
+  });
 });
